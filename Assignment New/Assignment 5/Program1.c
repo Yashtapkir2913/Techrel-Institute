3691dc52626cdfdd81d1c3f939419da4cd553fc8
Yashtapkir2913 <yashsantoshtapkir2913@gmail.com>
2025-08-27 20:42:15 +0530

    Pattern

---
+++ Assignment New/Assignment 5/Program1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    for(int i=0; i<5; i++) {         
+        for(int j=0; j<5; j++) {     
+            if(j % 2 == 0)
+                printf("0");
+            else
+                printf("1");
+        }
+        printf("\n");  
+    }
+    return 0;
+}
+
+/*
+01010
+01010
+01010
+01010
+01010
+*/
